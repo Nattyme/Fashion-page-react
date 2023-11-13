6dcd715e2ec01693e3b531175cb9a5a13050b4ed
NeuralNat <ifrance@yandex.ru>
2023-11-13 23:49:17 +0300

    header logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
+import logoImg from './../../img/icons/logo.svg'
 import './header.css';
 
 function Header () {
     return (
-        <header className = "header">
-            LOGO & NAV
+        <header className="header">
+            <div className="container">
+                <div className="header__row">
+                    <div className="header__logo">
+                        <img src={logoImg} alt="Logo" />
+                        <span>Fashion</span>
+                    </div>
+                    <div className="header__nav">NAV</div>
+                </div>
+            </div>
         </header>
     )
 }

5b1acee66ef9a7a41a62bd158cea9045e1eb0328
NeuralNat <ifrance@yandex.ru>
2023-11-13 23:16:51 +0300

    base header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
     <div className="App">
       <h1>Fashion</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header () {
+    return (
+        <header className = "header">
+            LOGO & NAV
+        </header>
+    )
+}
+
+export default Header;

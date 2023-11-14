0d5b1a33623ff9fbe927aaa6cbd5c76f635b0bfc
NeuralNat <ifrance@yandex.ru>
2023-11-14 15:03:16 +0300

    .brands HTML, style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header.jsx";
 import Promo from "./components/promo/Promo.jsx";
+import Brands from "./components/brands/Brands.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Promo />
+      {/* <Promo /> */}
+      <Brands />
     </div>
   );
 }

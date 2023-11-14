76a7d6179423d95a5183255b57957b9155fb30f3
NeuralNat <ifrance@yandex.ru>
2023-11-14 09:27:36 +0300

    .promo HTML final, content

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,19 +1,29 @@
-import './promo.css';
-import promoImg from './../../img/images/header-img.jpg';
+import "./promo.css";
+import promoImg from "./../../img/images/header-img.jpg";
 
 const Promo = () => {
-    return (
-        <section className="promo">
-            <div className="container">
-                <div className="promo__content">
-                    <div className="promo__text">TEXT</div>
-                    <div className="promo__img">
-                        <img src={promoImg} alt="Promo" />
-                    </div>
-                </div>
+  return (
+    <section className="promo">
+      <div className="container">
+        <div className="promo__content">
+          <div className="promo__text">
+            <div className="promo__title">LET’S EXPLORE UNIQUE CLOTHES.</div>
+            <div className="promo__disc">
+              Live for Influential and Innovative fashion!
             </div>
-        </section>
-    );
-}
+            <div className="promo__btn-wrapper">
+              <a href="#!" className="promo__btn">
+                Shop Now
+              </a>
+            </div>
+          </div>
+          <div className="promo__img">
+            <img src={promoImg} alt="Promo" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Promo;

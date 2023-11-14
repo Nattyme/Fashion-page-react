078a92f419d071055723c3a09a9a3ab42a46b8e1
NeuralNat <ifrance@yandex.ru>
2023-11-14 09:12:56 +0300

    .promo: HTML, base style

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,19 @@
+import './promo.css';
+import promoImg from './../../img/images/header-img.jpg';
+
+const Promo = () => {
+    return (
+        <section className="promo">
+            <div className="container">
+                <div className="promo__content">
+                    <div className="promo__text">TEXT</div>
+                    <div className="promo__img">
+                        <img src={promoImg} alt="Promo" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Promo;

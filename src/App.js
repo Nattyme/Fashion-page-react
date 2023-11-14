8b97001b7c423eb7258b53092a2144557624c5a6
NeuralNat <ifrance@yandex.ru>
2023-11-14 15:28:46 +0300

    .arrivals base html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/header/Header.jsx";
 import Promo from "./components/promo/Promo.jsx";
 import Brands from "./components/brands/Brands.jsx";
+import Arrivals from "./components/arrivals/Arrivals.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <Promo /> */}
-      <Brands />
+      {/* <Brands /> */}
+      <Arrivals />
     </div>
   );
 }
---
+++ src/components/arrivals/Arrivals.jsx
@@ -0,0 +1,20 @@
+import "./arrivals.css";
+
+const Arrivals = () => {
+  return (
+    <section className="arrivals">
+      <div className="container">
+        <div className="arrivals__header">
+          <h2 className="title-2">New arrivals</h2>
+        </div>
+        <div className="arrivals__cards">
+            <div>Card1</div>
+            <div>Card2</div>
+            <div>Card3</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Arrivals;

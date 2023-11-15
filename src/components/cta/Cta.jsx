5c13a4f499801e7df443a2a2c22d4f81e02fc03e
NeuralNat <ifrance@yandex.ru>
2023-11-15 19:40:01 +0300

    .cta style

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -11,16 +11,15 @@ const Cta = () => {
           <div className="cta__text">
             Type your email down below and be young wild generation
           </div>
-          <div className="cta__form">
-            <div className="cta__input-wrapper">
-              <input
-                type="text"
-                className="cta__input"
-                placeholder="Add your email here"
-              />
-            </div>
-            <div className="cta__btn-wrapper">
-              <button className="cta__btn">SEND</button>
+
+          <div className="cta__form-wrapper">
+            <div className="cta__form">
+              <div className="form__input-wrapper">
+                <input className="form__input" type="text" placeholder="Add your email here" />
+              </div>
+              <div className="form__btn-wrapper">
+                <button className="form__btn">SEND</button>
+              </div>
             </div>
           </div>
         </div>

cfd81efd42066851237f4b18823051959d0b462e
NeuralNat <ifrance@yandex.ru>
2023-11-15 19:58:38 +0300

    .footer HTML correction

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,7 +20,25 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="footer__nav">
-                       
+                       <ul className="nav__company">
+                        <li className="nav__title"><a href="#!">Company</a></li>
+                        <li><a href="#!">About</a></li>
+                        <li><a href="#!">Contact us</a></li>
+                        <li><a href="#!">Support</a></li>
+                        <li><a href="#!">Careers</a></li>
+                       </ul>
+                       <ul className="nav__link">
+                       <li className="nav__title">Quick Link<a href="#!"></a></li>
+                        <li><a href="#!">Share Location</a></li>
+                        <li><a href="#!">Orders Tracking</a></li>
+                        <li><a href="#!">Size Guide</a></li>
+                        <li><a href="#!">FAQs</a></li>
+                       </ul>
+                       <ul className="nav__legal">
+                       <li className="nav__title">Legal<a href="#!"></a></li>
+                        <li><a href="#!">Terms & conditions</a></li>
+                        <li><a href="#!">Privacy Policy</a></li>
+                       </ul>
                     </div>
                 </div>
             </div>

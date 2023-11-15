b9b50b73b682f12c67fba011252343fd339570a8
NeuralNat <ifrance@yandex.ru>
2023-11-15 12:07:18 +0300

    .payday base HTML

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Header from "./components/header/Header.jsx";
 import Promo from "./components/promo/Promo.jsx";
 import Brands from "./components/brands/Brands.jsx";
 import Arrivals from "./components/arrivals/Arrivals.jsx";
+import Payday from "./components/payday/Payday.jsx";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Promo />
+      {/* <Promo />
       <Brands />
-      <Arrivals />
+      <Arrivals /> */}
+      <Payday />
     </div>
   );
 }
---
+++ src/components/payday/Payday.jsx
@@ -0,0 +1,29 @@
+import "./payday.css";
+import paydayImg from "./../../img/images/payday-img.jpg";
+
+const Payday = () => {
+  return (
+    <section className="payday">
+      <div className="container">
+        <div className="payday__content">
+          <div className="payday__img">
+            <img src={paydayImg} alt="" />
+          </div>
+          <div className="payday__text">
+            <h2 className="payday__title">PAYDAY SALE NOW</h2>
+            <div className="payday__disc">
+              Spend minimal $100 get 30% off voucher code for your next purchase
+            </div>
+            <div className="payday__term">
+                <h3>1 June - 10 June 2021</h3>
+                <p>*Terms & Conditions apply</p>
+                </div>
+            <div className="payday__btn-wrapper"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Payday;

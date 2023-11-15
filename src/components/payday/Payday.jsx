e1cd992a1fca4e3177a0db32ecaa95e7038fcf85
NeuralNat <ifrance@yandex.ru>
2023-11-15 14:46:00 +0300

    .payday style

--- src/components/payday/Payday.jsx
+++ src/components/payday/Payday.jsx
@@ -10,7 +10,9 @@ const Payday = () => {
             <img src={paydayImg} alt="" />
           </div>
           <div className="payday__text">
-            <h2 className="payday__title">PAYDAY SALE NOW</h2>
+            <h2 className="payday__title"><span className="highlight highlight--whiteLess">
+                <span>PAYDAY</span>
+                </span>SALE NOW</h2>
             <div className="payday__disc">
               Spend minimal $100 get 30% off voucher code for your next purchase
             </div>
@@ -18,7 +20,9 @@ const Payday = () => {
                 <h3>1 June - 10 June 2021</h3>
                 <p>*Terms & Conditions apply</p>
                 </div>
-            <div className="payday__btn-wrapper"></div>
+            <div className="payday__btn-wrapper">
+                <a href="#!" className="payday__btn">SHOP NOW</a>
+            </div>
           </div>
         </div>
       </div>
